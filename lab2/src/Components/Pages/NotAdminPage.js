e29d6ab92944eedee11e7e00be097597a0ffbf57
ilirselmanii <selmani.alb123@gmail.com>
2025-01-23 16:23:46 +0100

    NotAdmin

---
+++ lab2/src/Components/Pages/NotAdminPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotAdminPage() {
+  return (
+    <div className='nadmin'>
+        <h1>No Access</h1>
+        <p>You are not an Admin</p>
+    </div>
+  )
+}
+
+export default NotAdminPage
